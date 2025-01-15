380beb77db0bed25e4fad20e51737721baa41b7a
Kainom <eleuteriokaina@gmail.com>
2025-01-15 16:07:42 -0300

    I have been added the security config

--- src/main/java/com/kainom/shop/config/WebClientConfig.java
+++ src/main/java/com/kainom/shop/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.kainom.shop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,16 +8,20 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${API_USER:http://localhost:8000/user}")
+    private String userApiURL;
+
+    @Value("${API_PRODUCT:http://localhost:8001/product}")
+    private String productApiURL;
 
     @Bean(name = "user")
-    public WebClient  webClientUser(WebClient.Builder builder){
-        return builder.baseUrl("http://localhost:8000/user").build();
+    public WebClient webClientUser(WebClient.Builder builder) {
+        return builder.baseUrl(userApiURL).build();
     }
 
-    @Bean(name="product")
-    public WebClient webClientProduct(WebClient.Builder builder){
-        return builder.baseUrl("http://localhost:8001/product").build();
+    @Bean(name = "product")
+    public WebClient webClientProduct(WebClient.Builder builder) {
+        return builder.baseUrl(productApiURL).build();
     }
 
-    
 }
