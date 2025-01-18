c687f2e88e7d4542a6be5ca604ed3c5c2207b4e6
Kainom <eleuteriokaina@gmail.com>
2025-01-18 17:58:04 -0300

    I added the modifications to contanaizer my application.i do this with maven.The docker compose files are  in  in my root dir

--- src/main/java/com/kainom/shop/ShopServiceApplication.java
+++ src/main/java/com/kainom/shop/ShopServiceApplication.java
@@ -3,10 +3,13 @@ package com.kainom.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ShopServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ShopServiceApplication.class, args);
 	}
 
